6d05fa9a2861f86fbbd43f394a8bcf6ffae6c218
SublimeDevv <juanmen1404@gmail.com>
2024-03-10 21:31:35 -0500

    socket.io-client

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import io from "socket.io-client";
 
-export default function App() {
+const App = () => {
+  const [sensorData, setSensorData] = useState([]);
+
+  useEffect(() => {
+    const socket = io("http://192.168.1.7:3000");
+
+    socket.on("updateData", (updatedData) => {
+      setSensorData(updatedData);
+    });
+
+    socket.emit("getData");
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      {sensorData.map((sensor) => (
+        <View key={sensor.id} style={styles.sensorItem}>
+          <Text style={styles.sensorText}>Sensor ID: {sensor.sensorId}</Text>
+          <Text style={styles.sensorText}>
+            Estado: {sensor.isOccupied ? "Ocupado" : "Libre"}
+          </Text>
+        </View>
+      ))}
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+  },
+  sensorItem: {
+    backgroundColor: "#f9f9f9",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 5,
+    elevation: 3,
+  },
+  sensorText: {
+    fontSize: 16,
   },
 });
+
+export default App;
